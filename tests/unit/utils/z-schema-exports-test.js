5d89d4459c8570ad9961b8cbe8cf6c0bd1fd32d0
Matthew Dahl <dahl.matthew@gmail.com>
2016-08-01 16:15:03 -0700

    Fix lint error

--- tests/unit/utils/z-schema-exports-test.js
+++ tests/unit/utils/z-schema-exports-test.js
@@ -2,38 +2,40 @@ import {expect} from 'chai'
 import {beforeEach, describe, it} from 'mocha'
 import ZSchema from 'z-schema'
 
-it('z-schema is a single module that has been exported', function () {
-  expect(typeof ZSchema).to.equal('function')
-})
-
-;[
-  'getRegisteredFormats',
-  'registerFormat',
-  'setSchemaReader'
-]
-  .forEach((key) => {
-    it(`has ${key} method`, function () {
-      expect(typeof ZSchema[key]).to.equal('function')
-    })
-  })
-
-describe('new instance', function () {
-  let validator
-
-  beforeEach(function () {
-    validator = new ZSchema()
+describe('z-schema', function () {
+  it('is a single module that has been exported', function () {
+    expect(typeof ZSchema).to.equal('function')
   })
 
   ;[
-    'getLastError',
-    'getLastErrors',
-    'setRemoteReference',
-    'validate',
-    'validateSchema'
+    'getRegisteredFormats',
+    'registerFormat',
+    'setSchemaReader'
   ]
     .forEach((key) => {
       it(`has ${key} method`, function () {
-        expect(typeof validator[key]).to.equal('function')
+        expect(typeof ZSchema[key]).to.equal('function')
       })
     })
+
+  describe('new instance', function () {
+    let validator
+
+    beforeEach(function () {
+      validator = new ZSchema()
+    })
+
+    ;[
+      'getLastError',
+      'getLastErrors',
+      'setRemoteReference',
+      'validate',
+      'validateSchema'
+    ]
+      .forEach((key) => {
+        it(`has ${key} method`, function () {
+          expect(typeof validator[key]).to.equal('function')
+        })
+      })
+  })
 })
