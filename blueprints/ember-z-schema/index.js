fb39ca0b331a2c7ddee7dcaec4d0194e60327e7f
Matthew Dahl <dahl.matthew@gmail.com>
2016-12-02 16:38:08 -0800

    Upgrade to latest z-schema

--- blueprints/ember-z-schema/index.js
+++ blueprints/ember-z-schema/index.js
@@ -1,13 +1,13 @@
 module.exports = {
   afterInstall: function () {
     return this.addPackagesToProject([
-      {name: 'z-schema', target: '3.18.1'}
+      {name: 'z-schema', target: '3.18.2'}
     ])
       .then(() => {
         return this.addAddonsToProject({
           packages: [
             {name: 'ember-lodash-shim', target: '^1.0.0'},
-            {name: 'ember-validator-shim', target: '^1.0.0'}
+            {name: 'ember-validator-shim', target: '^2.0.0'}
           ]
         })
       })
