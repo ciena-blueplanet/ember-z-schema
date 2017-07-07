aa4664d27276bc52cb80b91e3043da8512828d1c
Michael Carroll <juwara0@users.noreply.github.com>
2017-07-07 08:29:12 -0500

    code review changes

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -1,32 +1,49 @@
 /* eslint-env node */
+
 module.exports = function (environment) {
   var ENV = {
-    APP: {},
+    modulePrefix: 'dummy',
+    environment: environment,
     rootURL: '/',
+    locationType: 'hash',
     EmberENV: {
-      FEATURES: {},
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      },
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
         Date: false
       }
     },
-    environment: environment,
-    locationType: 'hash',
-    modulePrefix: 'dummy'
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    }
   }
 
-  switch (environment) {
-    case 'test':
-      // Testem prefers this...
-      ENV.rootURL = '/'
-      ENV.locationType = 'none'
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
 
-      // keep test console output quieter
-      ENV.APP.LOG_ACTIVE_GENERATION = false
-      ENV.APP.LOG_VIEW_LOOKUPS = false
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.locationType = 'none'
 
-      ENV.APP.rootElement = '#ember-testing'
-      break
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false
+    ENV.APP.LOG_VIEW_LOOKUPS = false
+
+    ENV.APP.rootElement = '#ember-testing'
+  }
+
+  if (environment === 'production') {
+
   }
 
   return ENV
